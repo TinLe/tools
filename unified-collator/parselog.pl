f76611578bc597726c5421d9026d3743fa4957e7
Tin Le <tin@le.org>
2013-01-04 16:54:53 -0800

    Do averaging, since diamond wants single value per param.

--- unified-collator/parselog.pl
+++ unified-collator/parselog.pl
@@ -18,26 +18,38 @@ my $YEAR = `date +"%Y"|tr -d '\r\n'`;
 my $totalrecords = 0;
 
 my ($day, $month, $year, $hour, $minute, $sec) = (0) x 6;
-my $data;
+my @data = {
+    _timing => 0.0,
+    audio => 0.0,
+    omr1 => 0.0,
+    omr2 => 0.0,
+    melody1 => 0.0,
+    melody2 => 0.0,
+    speech1 => 0.0,
+    speech2 => 0.0,
+    api => 0.0,
+    sing1 => 0.0,
+    sing2 => 0.0
+};
 
 my $helpflg = 0;
 
 sub Usage {
-	print STDERR << "EOF";
+  print STDERR << "EOF";
 
-	Usage: $progname [--help] logfile
-		--help      this help message.
-		logfile		logfile to analyze
+  Usage: $progname [--help] logfile
+    --help      this help message.
+    logfile    logfile to analyze
 
 EOF
-	exit;
+  exit;
 }
 
 ###################
 # Main
 
 if (!GetOptions(
-		"help|h" => \$helpflg,
+    "help|h" => \$helpflg,
 )) { Usage(); }
 
 if ($helpflg or !@ARGV) { Usage(); }
@@ -47,53 +59,64 @@ for ($i=0; $i<=$#ARGV; $i++) {
   open(MYFILE, $ARGV[$i]) or die("$progname: Unable to open file $ARGV[$i] for reading: $?\n");
   push(@logfiles, $ARGV[$i]);
   while (<MYFILE>) {
-	chomp;
+  chomp;
 
     # we are only interested in timing lines
-	next if !(/_timing/);
+  next if !(/_timing/);
 
-	$month = $data = '';
+  $month = '';
 #Jan  4 14:38:42 cy87bk1 unified_search_collator[2834]: _timing 9.5 audio 10.0 | omr 9.4 0.004 | melody 0.0 0.000 | speech 0.0 0.000 | api 0.142 | sing -1.000 (0)
-	if (m#^(\w+)\s+(\d+)\s+(\d\d):(\d\d):(\d\d)\s+([\w\d]+?)\s+unified_search_collator\[\d+\]: _timing (\d+\.\d+) audio (\d+\.\d+) \| omr (\d+\.\d+) (\d+\.\d+) \| melody (\d+\.\d+) (\d+\.\d+) \| speech (\d+\.\d+) (\d+\.\d+) \| api (\d+\.\d+) \| sing (-?\d+\.\d+) \((\d+)\)\s+$#) {
+    if (m#^(\w+)\s+(\d+)\s+(\d\d):(\d\d):(\d\d)\s+([\w\d]+?)\s+unified_search_collator\[\d+\]: _timing (\d+\.\d+) audio (\d+\.\d+) \| omr (\d+\.\d+) (\d+\.\d+) \| melody (\d+\.\d+) (\d+\.\d+) \| speech (\d+\.\d+) (\d+\.\d+) \| api (\d+\.\d+) \| sing (-?\d+\.\d+) \((\d+)\)\s+$#) {
 
-		$month = $1;
-		$day = $2;
-		$hour = $3;
-		$minute = $4;
-		$sec = $5;
-		$hostname = $6;
-		$timing = $7;
-		$audio = $8;
-		$omr1 = $9;
-		$omr2 = $10;
-		$melody1 = $11;
-		$melody2 = $12;
-		$speech1 = $13;
-		$speech2 = $14;
-		$api = $15;
-		$sing1 = $16;
-		$sing2 = $17;
+      $month = $1;
+      $day = $2;
+      $hour = $3;
+      $minute = $4;
+      $sec = $5;
+      #$hostname = $6;
+      $timing = $7;
+      $audio = $8;
+      $omr1 = $9;
+      $omr2 = $10;
+      $melody1 = $11;
+      $melody2 = $12;
+      $speech1 = $13;
+      $speech2 = $14;
+      $api = $15;
+      $sing1 = $16;
+      $sing2 = $17;
 
-		my $datetime = "$day $month $YEAR $hour:$minute:$sec";
-		my $time = (str2time($datetime) ? str2time($datetime): 0);
+      $totalrecords++;
 
-        printf("timing.%s.%s.melodis.com %f\n", $host, $datacenter, $timing);
-        printf("audio.%s.%s.melodis.com %f\n", $host, $datacenter, $audio);
-        printf("omr1.%s.%s.melodis.com %f\n", $host, $datacenter, $omr1);
-        printf("omr2.%s.%s.melodis.com %f\n", $host, $datacenter, $omr2);
-        printf("melody1.%s.%s.melodis.com %f\n", $host, $datacenter, $melody1);
-        printf("melody2.%s.%s.melodis.com %f\n", $host, $datacenter, $melody2);
-        printf("speech1.%s.%s.melodis.com %f\n", $host, $datacenter, $speech1);
-        printf("speech2.%s.%s.melodis.com %f\n", $host, $datacenter, $speech2);
-        printf("api.%s.%s.melodis.com %f\n", $host, $datacenter, $api);
-        printf("sing1.%s.%s.melodis.com %f\n", $host, $datacenter, $sing1);
-        printf("sing2.%s.%s.melodis.com %f\n", $host, $datacenter, $sing2);
-	}
+      $data{_timing} += $timing;
+      $data{audio} += $audio;
+      $data{omr1} += $omr1;
+      $data{omr2} += $omr2;
+      $data{melody1} += $melody1;
+      $data{melody2} += $melody2;
+      $data{speech1} += $speech1;
+      $data{speech2} += $speech2;
+      $data{api} += $api;
+      $data{sing1} += $sing1;
+      $data{sing2} += $sing2;
+
+      # my $datetime = "$day $month $YEAR $hour:$minute:$sec";
+      # my $time = (str2time($datetime) ? str2time($datetime): 0);
+    }
   }
   close(MYFILE) or die("Error closing file $ARGV[0]! $?\n");
-}
 
-my $startdate = scalar gmtime($ts_start);
-my $enddate = scalar gmtime($ts_end);
+  printf("servers.com.melodis.%s.%s.timing %f\n", $datacenter, $host, $data{_timing}/$totalrecords);
+  printf("servers.com.melodis.%s.%s.audio %f\n", $datacenter, $host, $data{audio}/$totalrecords);
+  printf("servers.com.melodis.%s.%s.omr1 %f\n", $datacenter, $host, $data{omr1}/$totalrecords);
+  printf("servers.com.melodis.%s.%s.omr2 %f\n", $datacenter, $host, $data{omr2}/$totalrecords);
+  printf("servers.com.melodis.%s.%s.melody1 %f\n", $datacenter, $host, $data{melody1}/$totalrecords);
+  printf("servers.com.melodis.%s.%s.melody2 %f\n", $datacenter, $host, $data{melody2}/$totalrecords);
+  printf("servers.com.melodis.%s.%s.speech1 %f\n", $datacenter, $host, $data{speech1}/$totalrecords);
+  printf("servers.com.melodis.%s.%s.speech2 %f\n", $datacenter, $host, $data{speech2}/$totalrecords);
+  printf("servers.com.melodis.%s.%s.api %f\n", $datacenter, $host, $data{api}/$totalrecords);
+  printf("servers.com.melodis.%s.%s.sing1 %f\n", $datacenter, $host, $data{sing1}/$totalrecords);
+  printf("servers.com.melodis.%s.%s.sing2 %f\n", $datacenter, $host, $data{sing2}/$totalrecords);
+}
 
 1;
