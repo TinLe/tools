b0a030470c3a477b0484df1a8b1b66d0cf85d81d
Tin Le <tin@le.org>
2018-06-13 18:40:54 -0700

    Add ES curator actions
    Move riak and mongo tools into subdir nosql

---
+++ elasticsearch/cronjobs/optimize.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# remove all indices older than 30 days
+#curator delete indices --older-than 60 --time-unit days --timestring '%Y.%m.%d'
+
+# remove all indices older than 9 weeks
+# curator delete indices --older-than 9 --time-unit weeks --timestring '%Y.%w'
+
+# optimize all indices older than today(current)
+#curator optimize indices --older-than 1 --time-unit days --timestring '%Y.%m.%d'
+curator ~tin/.curator/actions/forcemerge
+
+# remove all indices older than 3 month
+#curator delete indices --older-than 3 --time-unit months --timestring '%Y.%m'
+curator ~tin/.curator/actions/delete_indices_3months
+
+# remove all indices older than 7 days
+# curator delete indices --older-than 7 --time-unit days --timestring '%Y.%m.%d'
+curator ~tin/.curator/actions/delete_indices_7days
